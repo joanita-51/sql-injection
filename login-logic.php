e967eb0d5e13f3d650dfa266e45b96df6176b747
joanita-51 <nakityojoanita51@gmail.com>
2023-04-13 10:57:50 +0300

    final authentication

--- login-logic.php
+++ login-logic.php
@@ -9,10 +9,18 @@ $sql="SELECT * FROM users WHERE username='{$username}'";
 $mysqli = new mysqli("localhost","root","","my_bank");
 $res = $mysqli->query($sql);
 if($res->num_rows < 1){
-    header('Location:login.php?error=Wrong username');
+    header('Location:login.php?error=Wrong username'); 
     die();
 }
 
+$row = $res->fetch_assoc();
+$password_hash = $row['password'];
+if(password_verify($password, $password_hash) != true){
+    header('Location:login.php?error=Wrong password'); 
+    die();   
+}
+die("success");
 echo "<pre>";
-print_r($res->num_rows);
+print_r($row);
+// print_r($res->num_rows);
 // die($sql);
